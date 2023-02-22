108d142c6b7a3e27d2ed30e7bba7d50663f0305a
hecker7734 <heckerishere7735@gmail.com>
2023-02-22 15:18:56 -0600

    trackingMouse

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+var canvas = document.getElementById("canvas")
+var ctx = canvas.getContext("2d");
+var mousedown = false
+var customattack = []
+var Mousex = 0
+var Mousey = 0
+canvas.addEventListener("mousedown", function(){
+    mousedown = true
+});
+canvas.addEventListener("mouseup", function(){
+    mousedown = false
+});
+
+function generateFor(fortype) {
+    var genVal = setInterval(function() {
+        var img = document.getElementById(fortype);
+        ctx.drawImage(img, Mousex, Mousey);
+    },100)
+}
+
+function getPos(e){
+    Mousex=e.clientX;
+    Mousey=e.clientY;
+}
