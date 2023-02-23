89541dbbf5a12978e752a4eff1ffbd692a43a580
hecker7734 <91642757+hecker7734@users.noreply.github.com>
2023-02-22 22:32:21 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -155,7 +155,7 @@ function setbox() {
 function box() {
     ctx.strokeStyle = "white"
     ctx.lineWidth = 4
-    ctx.strokeRect(boxcoords[0],boxcoords[1]+ 100,boxcoords[2],boxcoords[3]+ 100)
+    ctx.strokeRect(boxcoords[0],boxcoords[1]+ 50,boxcoords[2],boxcoords[3]+ 50)
 }
 box()
 
